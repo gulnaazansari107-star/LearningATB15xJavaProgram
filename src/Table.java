4c60faf8ee1aae24c714ddb0e027d6a4cbe72ced
Gulnaaz Ansari <gulnaazansari107@gmail.com>
2025-12-17 22:04:32 +0530

    Updating my first file

---
+++ src/Table.java
@@ -0,0 +1,15 @@
+public class Table {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("%d x 1 = %d%n", a, a);
+        System.out.printf("%d x 2 = %d%n", a, (a*2));
+        System.out.printf("%d x 3 = %d%n", a, (a*3));
+        System.out.printf("%d x 4 = %d%n", a, (a*4));
+        System.out.printf("%d x 5 = %d%n", a, (a*5));
+        System.out.printf("%d x 6 = %d%n", a, (a*6));
+        System.out.printf("%d x 7 = %d%n", a, (a*7));
+        System.out.printf("%d x 8 = %d%n", a, (a*8));
+        System.out.printf("%d x 9 = %d%n", a, (a*9));
+        System.out.printf("%d x 10 = %d%n", a, (a*10));
+    }
+}
