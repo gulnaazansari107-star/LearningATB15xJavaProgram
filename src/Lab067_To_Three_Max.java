fd878697c5e2ac94ad8c55ccdbcc9d93be700eba
Gulnaaz Ansari <gulnaazansari107@gmail.com>
2025-12-25 18:50:18 +0530

    Updating on 25/12/25

---
+++ src/Lab067_To_Three_Max.java
@@ -0,0 +1,16 @@
+public class Lab067_To_Three_Max {
+
+        public static void main(String[] args) {
+
+            int n1 = 2;
+
+            int n2 = 9;
+
+            int n3 = -11;
+
+            int max = n1>n2 ? (n1>n3 ? n1:n3): (n2>n3? n2: n3);
+
+            System.out.println("MAX OUT OF THREE IS : " + max);
+
+        }
+}
